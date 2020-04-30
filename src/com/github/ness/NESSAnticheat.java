ef0ce8722ce4a50cd9da5bc447087e5f10c6117d
A248 <anandebeh@gmail.com>
2020-04-30 16:19:42 -0400

    Use single thread executor instead of NessScheduler

--- src/com/github/ness/NESSAnticheat.java
+++ src/com/github/ness/NESSAnticheat.java
@@ -1,24 +1,38 @@
 package com.github.ness;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.ness.listener.InteractionListener;
 
+import lombok.Getter;
+
 public class NESSAnticheat extends JavaPlugin {
 
-	private NessScheduler scheduler;
+	@Getter
+	private Executor executor;
 	
 	@Override
 	public void onEnable() {
 		PlayerManager manager = new PlayerManager();
-		scheduler = new NessScheduler();
-		getServer().getScheduler().runTaskAsynchronously(this, scheduler);
+		executor = Executors.newSingleThreadExecutor();
 		getServer().getPluginManager().registerEvents(new InteractionListener(manager), this);
 	}
 	
 	@Override
 	public void onDisable() {
-		scheduler.close();
+		ExecutorService service = ((ExecutorService) executor);
+		service.shutdown();
+		try {
+			service.awaitTermination(10L, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			ex.printStackTrace();
+		}
+		executor = null;
 	}
 	
 	
