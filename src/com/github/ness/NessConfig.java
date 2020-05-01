fed9c02cfb51ee3c5490c4b4b3da240d16dc5b0c
A248 <anandebeh@gmail.com>
2020-05-01 09:36:16 -0400

    Add stuff to NessConfig

--- src/com/github/ness/NessConfig.java
+++ src/com/github/ness/NessConfig.java
@@ -1,5 +1,18 @@
 package com.github.ness;
 
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 public class NessConfig {
 
+	private final YamlConfiguration config;
+	
+	NessConfig(YamlConfiguration config) {
+		this.config = config;
+	}
+	
+	public ConfigurationSection getCheck(String check) {
+		return config.getConfigurationSection("checks." + check);
+	}
+	
 }
