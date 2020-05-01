a575d8ee3750368dc86ff06626544b8725a2370a
A248 <anandebeh@gmail.com>
2020-05-01 09:36:32 -0400

    Begin working on CPS constancy check

--- src/com/github/ness/check/CPSCheck.java
+++ src/com/github/ness/check/CPSCheck.java
@@ -1,8 +1,11 @@
 package com.github.ness.check;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.github.ness.CheckManager;
@@ -11,25 +14,52 @@ import com.github.ness.Violation;
 
 public class CPSCheck extends AbstractCheck<PlayerInteractEvent> {
 
+	private int hardLimit;
+	private int hardLimitRetention;
+	private int constancyThreshold;
+	private int constancyRetention;
+	
 	public CPSCheck(CheckManager manager) {
 		super(manager, CheckInfo.eventWithAsyncPeriodic(PlayerInteractEvent.class, 4, TimeUnit.SECONDS));
+		ConfigurationSection section = manager.getNess().getNessConfig().getCheck("cps");
+		hardLimit = section.getInt("hard-limit.cps", 16);
+		hardLimitRetention = 1000 * section.getInt("hard-limit.retention-span", 4);
+		constancyThreshold = section.getInt("constancy.threshold", 4);
+		constancyRetention = 1000 * section.getInt("constancy.retention-span", 24);
 	}
-
-	/**
-	 * Retain clicks for 4 seconds in CPS check
-	 * 
-	 */
-	private static final int CLICK_HISTORY_RETENTION = 4;
 
 	@Override
 	void checkAsyncPeriodic(NessPlayer player) {
+		// Cleanup old history
 		Set<Long> clickHistory = player.getClickHistory();
-		long now = System.currentTimeMillis();
-		clickHistory.add(now);
-		clickHistory.removeIf((time) -> time - now > (CLICK_HISTORY_RETENTION * 1000L));
-		int clicks = clickHistory.size();
-		if (clicks / CLICK_HISTORY_RETENTION > 14) {
-			player.setViolation(new Violation("CPS", clicks));
+		long now1 = System.currentTimeMillis();
+		clickHistory.removeIf((time) -> time - now1 > constancyRetention);
+
+		if (clickHistory.isEmpty()) {
+			return; // Don't check players who aren't clicking
+		}
+
+		// Copy temporary state
+		Set<Long> copy1 = new HashSet<>(clickHistory);
+		ArrayList<Long> copy2 = new ArrayList<>(copy1);
+
+		// Hard limit check
+
+		long now2 = System.currentTimeMillis();
+		copy1.removeIf((time) -> time - now2 > hardLimitRetention);
+		int cps = copy1.size() / hardLimitRetention;
+		if (cps > hardLimit) {
+			player.setViolation(new Violation("CPS", cps));
+		}
+
+		// Constancy check
+
+		if (cps > constancyThreshold) {
+			copy2.sort(null);
+			long start = copy2.get(0);
+			long end = copy2.get(copy2.size() - 1);
+			ArrayList<Long> subClicks = new ArrayList<>();
+			
 		}
 	}
 
