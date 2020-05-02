edcabe917a2eb1a4e49bcc1acfa6861f957b0550
A248 <anandebeh@gmail.com>
2020-05-02 08:29:19 -0400

    Change AntiASCII to AntiUnicode and also make it more efficient

---
+++ src/com/github/ness/check/AntiUnicode.java
@@ -0,0 +1,26 @@
+package com.github.ness.check;
+
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import com.github.ness.CheckManager;
+import com.github.ness.Violation;
+
+public class AntiUnicode extends AbstractCheck<AsyncPlayerChatEvent>  {
+
+	private static final ThreadLocal<CharsetEncoder> asciiEncoder = ThreadLocal.withInitial(() -> StandardCharsets.US_ASCII.newEncoder());
+	
+	public AntiUnicode(CheckManager manager) {
+		super(manager, CheckInfo.eventOnly(AsyncPlayerChatEvent.class));
+	}
+
+	@Override
+	void checkEvent(AsyncPlayerChatEvent e) {
+		if (!asciiEncoder.get().canEncode(e.getMessage())) {
+			manager.getPlayer(e.getPlayer()).setViolation(new Violation("AntiUnicode", e.getMessage()));
+		}	
+	}
+
+}
