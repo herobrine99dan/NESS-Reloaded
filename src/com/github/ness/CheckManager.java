cc483ca56565403d3665320d21a677454ba4bd79
A248 <anandebeh@gmail.com>
2020-05-02 07:27:04 -0400

    Load checks from configuration

--- src/com/github/ness/CheckManager.java
+++ src/com/github/ness/CheckManager.java
@@ -1,5 +1,7 @@
 package com.github.ness;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -16,35 +18,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.github.ness.check.AbstractCheck;
-import com.github.ness.check.AntiASCII;
-import com.github.ness.check.AntiKb;
-import com.github.ness.check.AutoClicker;
-import com.github.ness.check.BadPackets;
-import com.github.ness.check.CPSCheck;
-import com.github.ness.check.Criticals;
-import com.github.ness.check.EntitySpeedCheck;
-import com.github.ness.check.FastEat;
-import com.github.ness.check.FastPlace;
-import com.github.ness.check.FastStairs;
-import com.github.ness.check.Fly;
-import com.github.ness.check.GhostHand;
-import com.github.ness.check.Headless;
-import com.github.ness.check.IllegalInteraction;
-import com.github.ness.check.InventoryHack;
-import com.github.ness.check.Jesus;
-import com.github.ness.check.Killaura;
-import com.github.ness.check.KillauraBotCheck;
-import com.github.ness.check.NoClip;
-import com.github.ness.check.NoSlowDownBow;
-import com.github.ness.check.NoSlowDownFood;
-import com.github.ness.check.NoSwingAnimation;
-import com.github.ness.check.NoSwingAttack;
-import com.github.ness.check.PatternKillauraAttack;
-import com.github.ness.check.PatternKillauraMove;
-import com.github.ness.check.Scaffold;
-import com.github.ness.check.SpamBot;
-import com.github.ness.check.Speed;
-import com.github.ness.check.Sprint;
 
 import lombok.Getter;
 
@@ -61,41 +34,33 @@ public class CheckManager implements AutoCloseable {
 		this.ness = ness;
 	}
 	
-	private void addCheck(AbstractCheck<?> check) {
-		check.initiatePeriodicTasks();
-		checks.add(check);
-	}
-	
 	void addAllChecks() {
-		addCheck(new CPSCheck(this));
-		addCheck(new AntiASCII(this));
-		addCheck(new AntiKb(this));
-		addCheck(new AutoClicker(this));
-		//addCheck(new BadPackets(this));
-		addCheck(new Criticals(this));
-		addCheck(new EntitySpeedCheck(this));
-		addCheck(new FastEat(this));
-		addCheck(new FastPlace(this));
-		//addCheck(new FastStairs(this));
-		addCheck(new Fly(this));
-		addCheck(new GhostHand(this));
-		addCheck(new Headless(this));
-		addCheck(new IllegalInteraction(this));
-		addCheck(new InventoryHack(this));
-		addCheck(new Jesus(this));
-		addCheck(new Killaura(this));
-		addCheck(new KillauraBotCheck(this));
-		addCheck(new NoClip(this));
-		addCheck(new NoSlowDownBow(this));
-		addCheck(new NoSlowDownFood(this));
-		addCheck(new NoSwingAnimation(this));
-		addCheck(new NoSwingAttack(this));
-		addCheck(new PatternKillauraAttack(this));
-		addCheck(new PatternKillauraMove(this));
-		addCheck(new Scaffold(this));
-		addCheck(new SpamBot(this));
-		addCheck(new Speed(this));
-		addCheck(new Sprint(this));
+		for (String checkName : ness.getNessConfig().getEnabledChecks()) {
+			if (!checkName.equals("AbstractCheck") && !checkName.equals("CheckInfo")) {
+				try {
+					Class<?> clazz = Class.forName("com.github.ness.check." + checkName);
+					if (AbstractCheck.class.isAssignableFrom(clazz)) {
+						Constructor<?> constructor = clazz.getDeclaredConstructor(CheckManager.class);
+						checks.add((AbstractCheck<?>) constructor.newInstance(this));
+					} else {
+						// error
+					}
+				} catch (ClassNotFoundException ex) {
+					// Later we will log better
+
+					// No class found!
+					// This is the user's fault
+					ex.printStackTrace();
+				} catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+						| IllegalArgumentException | InvocationTargetException ex) {
+
+					// Reflection error!
+					// This is our fault
+					ex.printStackTrace();
+				}
+			}
+		}
+		checks.forEach((check) -> check.initiatePeriodicTasks());
 	}
 	
 	void registerListener() {
