6133b8cbf11023da738b9a69166c1999fba4edec
A248 <anandebeh@gmail.com>
2020-05-02 09:08:25 -0400

    Cleanup LiquidInteraction and add it to the config

---
+++ src/com/github/ness/check/LiquidInteraction.java
@@ -0,0 +1,29 @@
+package com.github.ness.check;
+
+import java.util.List;
+
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import com.github.ness.CheckManager;
+import com.github.ness.Violation;
+
+public class LiquidInteraction extends AbstractCheck<BlockPlaceEvent>{
+	
+	private final List<String> whitelistedMaterials;
+	
+	public LiquidInteraction(CheckManager manager) {
+		super(manager, CheckInfo.eventOnly(BlockPlaceEvent.class));
+		whitelistedMaterials = manager.getNess().getNessConfig().getCheck(LiquidInteraction.class).getStringList("whitelisted-materials");
+	}
+	
+	@Override
+	void checkEvent(BlockPlaceEvent event) {
+		if (event.getBlockAgainst().isLiquid()) {
+			String type = event.getBlock().getType().name();
+			if (!whitelistedMaterials.contains(type)) {
+				manager.getPlayer(event.getPlayer()).setViolation(new Violation("LiquidInteraction", type));
+			}
+		}
+	}
+
+}
