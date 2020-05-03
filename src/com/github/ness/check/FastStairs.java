45f0413caa8043724d55f336f785140096547386
A248 <anandebeh@gmail.com>
2020-05-03 12:51:15 -0400

    Still more cleanup

--- src/com/github/ness/check/FastStairs.java
+++ src/com/github/ness/check/FastStairs.java
@@ -1,13 +1,6 @@
 package com.github.ness.check;
 
-import java.util.Arrays;
-import java.util.List;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerMoveEvent;
-import com.github.ness.MovementPlayerData;
-import com.github.ness.Utilities;
-import com.github.ness.Utility;
 
 public class FastStairs {
 
