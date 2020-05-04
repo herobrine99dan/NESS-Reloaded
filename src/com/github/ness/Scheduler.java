2f5c81919fc00ea6ed6066b968d561b950ceff0a
herobrine99dan <radiojukebox05@gmail.com>
2020-05-04 20:55:06 +0200

    Fourth Stage: New Scheduler Class

--- src/com/github/ness/Scheduler.java
+++ src/com/github/ness/Scheduler.java
@@ -4,6 +4,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitScheduler;
 
+import com.github.ness.check.InventoryHack;
+
 public class Scheduler {
 
 	public void start() {
@@ -12,7 +14,12 @@ public class Scheduler {
             @Override
             public void run() {
                 for(Player p: Bukkit.getOnlinePlayers()) {
-                	
+                	NessPlayer np = InventoryHack.manageraccess.getPlayer(p);
+                	np.setBlockplace(0);
+                	np.setClicks(0);
+                	np.setDrop(0);
+                	np.setOnMoveRepeat(0);
+                	np.setPackets(0);
                 }
             }
         }, 0L, 20L);
