d0ecf6ce53e3876edef95284d5939e38d195cd79
A248 <anandebeh@gmail.com>
2020-05-06 08:55:01 -0400

    Load NMSHandler in onEnable

--- src/main/java/com/github/ness/NESSAnticheat.java
+++ src/main/java/com/github/ness/NESSAnticheat.java
@@ -6,10 +6,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.ness.nms.NMSHandler;
 import com.github.ness.protocol.TinyProtocol;
 import com.github.ness.protocol.TinyProtocolListeners;
 
@@ -27,6 +29,8 @@ public class NESSAnticheat extends JavaPlugin {
 	public boolean devMode = true;
 	@Getter
 	private CheckManager checkManager;
+	@Getter
+	private NMSHandler nmsHandler;
 
 	@Override
 	public void onEnable() {
@@ -47,14 +51,40 @@ public class NESSAnticheat extends JavaPlugin {
 					"Your messages.yml is outdated! Until you regenerate it, NESS will use default values for some messages.");
 		}
 		getLogger().info("NESS-Reloaded includes code from NGXDEV.COM, which is licensed under the MIT license");
+
+		NMSHandler nmsHandler = findNMSHandler();
+		if (nmsHandler == null) {
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+		this.nmsHandler = nmsHandler;
+
 		executor = Executors.newSingleThreadScheduledExecutor();
 		getServer().getPluginCommand("ness").setExecutor(new NessCommands(this));
 		checkManager = new CheckManager(this);
+
 		this.protocol = (TinyProtocol) new TinyProtocolListeners((Plugin) this);
 		new Scheduler().start();
 		//new Protocols();
+
 		CompletableFuture<?> future = checkManager.loadAsync();
 		getServer().getScheduler().runTaskLater(this, future::join, 1L);
+	}
+	
+	private NMSHandler findNMSHandler() {
+		String packageName = Bukkit.getServer().getClass().getPackage().getName(); // org.bukkit.craftbukkit.v1_8_R3
+		String nmsVersion = packageName.substring("org.bukkit.craftbukkit.v".length()); // 1_8_R3
+		try {
+			Class<?> handlerClass = Class.forName("com.github.ness.nms.NMS_" + nmsVersion);
+			if (NMSHandler.class.isAssignableFrom(handlerClass)) {
+				return (NMSHandler) handlerClass.newInstance();
+			}
+		} catch (ClassNotFoundException ex) {
+			getLogger().warning("Your server's version (" + nmsVersion + ") is not recognised.");
+		} catch (InstantiationException | IllegalAccessException ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 	
 	@Override
