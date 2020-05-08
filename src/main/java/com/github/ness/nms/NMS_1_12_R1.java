29b4fa892f715416f3ac526467039e92fee97bb6
A248 <anandebeh@gmail.com>
2020-05-08 09:21:08 -0400

    Send the add player packet in NPC#completeSpawn

--- src/main/java/com/github/ness/nms/NMS_1_12_R1.java
+++ src/main/java/com/github/ness/nms/NMS_1_12_R1.java
@@ -47,6 +47,7 @@ public class NMS_1_12_R1 implements NMSHandler {
 				npc.triggerHealthUpdate();
 				PlayerConnection connection = ((CraftPlayer) getTargetPlayer()).getHandle().playerConnection;
 				connection.sendPacket(new PacketPlayOutEntityMetadata(npc.getId(), npc.getDataWatcher(), true));
+				connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, npc));
 				connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.UPDATE_LATENCY, npc));
 				connection.sendPacket(new PacketPlayOutNamedEntitySpawn(npc));
 				if (armor != null) {
