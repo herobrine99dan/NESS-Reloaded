60a654b75451325ac699f33c8706b8df7501c2ce
herobrine99dan <radiojukebox05@gmail.com>
2020-05-27 10:34:46 +0200

    Fourth Stage: Testing

--- src/main/java/com/github/ness/packetswrapper/SimplePacket.java
+++ src/main/java/com/github/ness/packetswrapper/SimplePacket.java
@@ -2,16 +2,17 @@ package com.github.ness.packetswrapper;
 
 public class SimplePacket {
 	private Object packet;
+
 	public SimplePacket(Object packet) {
 		this.packet = packet;
 	}
-	
+
 	public String getName() {
 		return this.packet.getClass().getSimpleName();
 	}
-	
+
 	public Object getPacket() {
 		return this.packet;
 	}
-	
+
 }
--- src/main/java/com/github/ness/protocol/NewPacketListener.java
+++ src/main/java/com/github/ness/protocol/NewPacketListener.java
@@ -1,5 +1,6 @@
 package com.github.ness.protocol;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,6 +8,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.github.ness.nms.ReflectionUtility;
+import com.github.ness.packetswrapper.PacketPlayInFlying;
+import com.github.ness.packetswrapper.PacketPlayInPositionLook;
+import com.github.ness.packetswrapper.PacketPlayInUseEntity;
+import com.github.ness.packetswrapper.SimplePacket;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelDuplexHandler;
@@ -33,8 +38,7 @@ public class NewPacketListener implements Listener {
 		}
 	}
 
-	public void removePlayer(Player player)
-			throws Exception {
+	public void removePlayer(Player player) throws Exception {
 		Channel channel = getChannel(player);
 		// Channel channel = ((CraftPlayer)
 		// player).getHandle().playerConnection.networkManager.channel;
@@ -44,14 +48,12 @@ public class NewPacketListener implements Listener {
 		});
 	}
 
-	public void injectPlayer(Player player)
-			throws Exception {
+	public void injectPlayer(Player player) throws Exception {
 		ChannelDuplexHandler channelDuplexHandler = new ChannelDuplexHandler() {
 			@Override
 			public void channelRead(ChannelHandlerContext channelHandlerContext, Object packet) throws Exception {
-				String packetname = packet.toString().substring(0, packet.toString().indexOf("@"))
-						.replace("net.minecraft.server.", "");
-				player.sendMessage(packetname);//Example
+				SimplePacket packetconverted = convertPacket(player, packet);
+
 				super.channelRead(channelHandlerContext, packet);
 			}
 		};
@@ -62,17 +64,43 @@ public class NewPacketListener implements Listener {
 		pipeline.addBefore("packet_handler", player.getName(), channelDuplexHandler);
 	}
 
+	public SimplePacket convertPacket(Player p, Object packet) {
+		SimplePacket packetconverted = new SimplePacket(packet);
+		String packetname = packet.toString().substring(0, packet.toString().indexOf("@"))
+				.replace("net.minecraft.server.", "");
+		if (packetname.toLowerCase().contains("useentity")) {
+			PacketPlayInUseEntity entitypacket = new PacketPlayInUseEntity(packet);
+			p.sendMessage("EntityPacket: " + entitypacket.getAction() + " " + entitypacket.getEntityId());
+			packetconverted = entitypacket;
+			return packetconverted;
+		} else if (packetname.toLowerCase().contains("position")) {
+			PacketPlayInPositionLook positionpacket = new PacketPlayInPositionLook(packet);
+			p.sendMessage("PositionPacket: " + new Location(p.getWorld(), positionpacket.getX(), positionpacket.getY(),
+					positionpacket.getZ(), positionpacket.getYaw(), positionpacket.getPitch()));
+			packetconverted = positionpacket;
+			return packetconverted;
+		} else if (packetname.toLowerCase().contains("inflying")) {
+			PacketPlayInFlying flyingpacket = new PacketPlayInFlying(packet);
+			p.sendMessage("inFlyingPacket: " + flyingpacket.isOnGround());
+			packetconverted = flyingpacket;
+			return packetconverted;
+		} else {
+			return packetconverted;
+		}
+	}
+
 	public Channel getChannel(Player player) {
 		try {
-		Class<?> craftplayerclass = (Class<?>) Class
-				.forName("org.bukkit.craftbukkit." + ReflectionUtility.ver() + ".entity.CraftPlayer");
-		// Object craftedplayer = craftplayerclass.cast(player);
-		Object handle = craftplayerclass.getMethod("getHandle").invoke(player);
-		Object playerConnection = handle.getClass().getDeclaredField("playerConnection").get(handle);
-		Object networkManager = playerConnection.getClass().getDeclaredField("networkManager").get(playerConnection);
-		Channel channel = (Channel) networkManager.getClass().getDeclaredField("channel").get(networkManager);
-		return channel;
-		}catch(Exception ex) {
+			Class<?> craftplayerclass = (Class<?>) Class
+					.forName("org.bukkit.craftbukkit." + ReflectionUtility.ver() + ".entity.CraftPlayer");
+			// Object craftedplayer = craftplayerclass.cast(player);
+			Object handle = craftplayerclass.getMethod("getHandle").invoke(player);
+			Object playerConnection = handle.getClass().getDeclaredField("playerConnection").get(handle);
+			Object networkManager = playerConnection.getClass().getDeclaredField("networkManager")
+					.get(playerConnection);
+			Channel channel = (Channel) networkManager.getClass().getDeclaredField("channel").get(networkManager);
+			return channel;
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
 		}
