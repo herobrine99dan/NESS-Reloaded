eee1987935ea307d8a20d7cd24b0259de0d56a48
A248 <anandebeh@gmail.com>
2020-06-02 18:02:31 -0400

    Add TestableCheckManager
    May be useful later

---
+++ src/test/java/com/github/ness/TestableCheckManager.java
@@ -0,0 +1,28 @@
+package com.github.ness;
+
+/**
+ * Can be useful to instantiate checks during test
+ * 
+ * @author A248
+ *
+ */
+public class TestableCheckManager extends CheckManager {
+
+	private static volatile TestableCheckManager inst;
+	
+	private TestableCheckManager() {
+		super(null);
+	}
+	
+	public static TestableCheckManager get() {
+		if (inst == null) {
+			synchronized (TestableCheckManager.class) {
+				if (inst == null) {
+					inst = new TestableCheckManager();
+				}
+			}
+		}
+		return inst;
+	}
+
+}
