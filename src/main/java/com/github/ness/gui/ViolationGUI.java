e113e27d43d2ef969debab9be5ae866596e728cf
herobrine99dan <radiojukebox05@gmail.com>
2020-06-18 17:47:31 +0200

    Fifth Stage: Starting the fix to view cheats and vl

--- src/main/java/com/github/ness/gui/ViolationGUI.java
+++ src/main/java/com/github/ness/gui/ViolationGUI.java
@@ -1,14 +1,10 @@
 package com.github.ness.gui;
 
-import java.util.Arrays;
-
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import com.github.ness.NESSAnticheat;
@@ -32,45 +28,41 @@ public class ViolationGUI {
 					p.sendMessage("This player isn't online!");
 					return;
 				}
-				showViolations(Bukkit.getPlayer(event.getName()), p);
 			}
 		}, NESSAnticheat.main);
 		int i = 0;
 		for (Player cheater : Bukkit.getOnlinePlayers()) {
 			i++;
-			menu.setOption(i - 1, getPlayerHead(cheater), cheater.getName(),
-					"Cheats: " + NESSAnticheat.main.getCheckManager().getPlayer(p).checkViolationCounts.size() + "");
+			String flaggedchecks = "Cheats:";
+			NessPlayer np = NESSAnticheat.main.getCheckManager().getPlayer(cheater);
+			for (String s : np.checkViolationCounts.keySet()) {
+				flaggedchecks = flaggedchecks + System.lineSeparator();
+				flaggedchecks = flaggedchecks + s + " VL: " + np.checkViolationCounts.getOrDefault(s, 0);
+			}
+			menu.setOption(i - 1, getPlayerHead(cheater), cheater.getName(), flaggedchecks);
 		}
 		menu.open(p);
 	}
 
-	public void showViolations(Player p, Player sender) {
-
+	public void showViolations(Player p, IconMenu menu) {
 		/*
-		 * IconMenu violationmenu = new IconMenu("§c§lViolations for " + p.getName(), 9,
-		 * new IconMenu.OptionClickEventHandler() {
-		 * 
-		 * @Override public void onOptionClick(OptionClickEvent event) {
-		 * 
-		 * } }, NESSAnticheat.main); int i = 0; for (String s :
-		 * NESSAnticheat.main.getCheckManager().getPlayer(p).checkViolationCounts.keySet
-		 * ()) { i++; violationmenu.setOption(i - 1, new
-		 * ItemStack(Material.REDSTONE_BLOCK, 1, (short) 0), s); }
-		 * violationmenu.open(sender);
+		 * Inventory myInventory = Bukkit.createInventory(null, 9, "§c§lViolations for "
+		 * + p.getName()); int i = 0; NessPlayer np =
+		 * NESSAnticheat.main.getCheckManager().getPlayer(p); for (String s :
+		 * np.checkViolationCounts.keySet()) { i++; ItemStack item = new
+		 * ItemStack(Material.REDSTONE_BLOCK, 1); ItemMeta metadata =
+		 * item.getItemMeta(); metadata.setDisplayName(s);
+		 * metadata.setLore(Arrays.asList("VL: " +
+		 * np.checkViolationCounts.getOrDefault(s, 0))); item.setItemMeta(metadata);
+		 * myInventory.setItem(i - 1, item); } sender.openInventory(myInventory);
 		 */
-		Inventory myInventory = Bukkit.createInventory(null, 9, "§c§lViolations for " + p.getName());
-		int i = 0;
 		NessPlayer np = NESSAnticheat.main.getCheckManager().getPlayer(p);
+		int i = 0;
 		for (String s : np.checkViolationCounts.keySet()) {
 			i++;
-			ItemStack item = new ItemStack(Material.REDSTONE_BLOCK, 1);
-			ItemMeta metadata = item.getItemMeta();
-			metadata.setDisplayName(s);
-			metadata.setLore(Arrays.asList("VL: " + np.checkViolationCounts.getOrDefault(s, 0)));
-			item.setItemMeta(metadata);
-			myInventory.setItem(i - 1, item);
+			menu.setOption(i - 1, new ItemStack(Material.REDSTONE_BLOCK, 1), s,
+					"VL: " + np.checkViolationCounts.getOrDefault(s, 0));
 		}
-		sender.openInventory(myInventory);
 	}
 
 	public ItemStack getPlayerHead(Player player) {
