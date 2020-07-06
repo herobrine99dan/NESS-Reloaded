5d763a74efdc2228abc36d0800747ccae720672b
herobrine99dan <radiojukebox05@gmail.com>
2020-07-06 12:03:15 +0200

    Making FastSneak Check

---
+++ src/main/java/com/github/ness/check/FastSneak.java
@@ -0,0 +1,43 @@
+package com.github.ness.check;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import com.github.ness.CheckManager;
+import com.github.ness.NessPlayer;
+import com.github.ness.api.Violation;
+import com.github.ness.utility.Utility;
+
+public class FastSneak extends AbstractCheck<PlayerMoveEvent> {
+
+	public FastSneak(CheckManager manager) {
+		super(manager, CheckInfo.eventOnly(PlayerMoveEvent.class));
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void checkEvent(PlayerMoveEvent e) {
+		Check(e);
+		Check1(e);
+	}
+
+	void Check(PlayerMoveEvent e) {
+		Player p = e.getPlayer();
+		NessPlayer np = this.manager.getPlayer(p);
+		if (p.isSneaking() && !Utility.hasflybypass(p) && !p.isFlying()) {
+			np.setViolation(new Violation("FastSneak", "Sneaking while Sprinting"));
+			np.shouldCancel(e, "FastSneak");
+		}
+	}
+
+	void Check1(PlayerMoveEvent e) {
+		Player p = e.getPlayer();
+		NessPlayer np = this.manager.getPlayer(p);
+		if (p.isSneaking() && !Utility.hasflybypass(p) && !p.isSprinting()) {
+			if (Utility.getMaxSpeed(e.getFrom(), e.getTo()) > 0.15) {
+				np.setViolation(new Violation("FastSneak", "HighDistance"));
+				np.shouldCancel(e, "FastSneak");
+			}
+		}
+	}
+}
