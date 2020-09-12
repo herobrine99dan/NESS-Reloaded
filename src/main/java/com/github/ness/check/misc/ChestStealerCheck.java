ffed653d2313c0e50daead9f9bca53ded837b808
MatuloM <71179721+MatuloM@users.noreply.github.com>
2020-09-12 18:49:17 +0200

    Update ChestStealerCheck.java

--- src/main/java/com/github/ness/check/misc/ChestStealerCheck.java
+++ src/main/java/com/github/ness/check/misc/ChestStealerCheck.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.Material;
 
 import com.github.ness.CheckManager;
 import com.github.ness.NessPlayer;
