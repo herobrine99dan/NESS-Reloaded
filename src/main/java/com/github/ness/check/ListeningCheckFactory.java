4176f2de9c249fd1515bc7ef1a68f196605c624b
A248 <anandebeh@gmail.com>
2020-09-19 14:38:30 -0400

    Add even more debugging to executing checks

--- src/main/java/com/github/ness/check/ListeningCheckFactory.java
+++ src/main/java/com/github/ness/check/ListeningCheckFactory.java
@@ -5,7 +5,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.github.ness.NessLogger;
 import com.github.ness.packets.ReceivedPacketEvent;
 import com.github.ness.utility.HandlerListUtils;
 import com.github.ness.utility.UncheckedReflectiveOperationException;
@@ -18,7 +21,9 @@ class ListeningCheckFactory<E extends Event, C extends AbstractCheck<E>> extends
 
 	private final ScalableRegisteredListener<E> scalableListener;
 	private final Class<E> eventClass;
-	private final Function<E, Player> getPlayerFunction;
+	private final Function<E, UUID> getPlayerFunction;
+	
+	private static final Logger logger = NessLogger.getLogger(ListeningCheckFactory.class);
 	
 	ListeningCheckFactory(Constructor<C> constructor, CheckManager manager, CheckInfo<E> checkInfo) {
 		super(constructor, manager, checkInfo);
@@ -32,14 +37,18 @@ class ListeningCheckFactory<E extends Event, C extends AbstractCheck<E>> extends
 	}
 	
 	void checkEvent(E event) {
+		logger.log(Level.FINEST, "Listening to event {0}", event);
+
 		if (getPlayerFunction != null) {
-			Player player = getPlayerFunction.apply(event);
-			UUID uuid = player.getUniqueId();
+			UUID uuid = getPlayerFunction.apply(event);
+			logger.log(Level.FINEST, "Found UUID {0} for event", uuid);
 			C check = getChecks().get(uuid);
 			if (check != null) {
+				logger.log(Level.FINEST, "Found check {0} for uuid in event", check);
 				check.checkEvent(event);
 			}
 		} else {
+			logger.fine("No UUID involved in event");
 			getChecks().values().forEach((check) -> check.checkEvent(event));
 		}
 		
@@ -57,12 +66,12 @@ class ListeningCheckFactory<E extends Event, C extends AbstractCheck<E>> extends
 		HandlerListUtils.getEventListeners(eventClass).unregister(scalableListener);
 	}
 	
-	private static <E extends Event> Function<E, Player> findGetPlayerFunction(Class<E> eventClass) {
+	private static <E extends Event> Function<E, UUID> findGetPlayerFunction(Class<E> eventClass) {
 		if (PlayerEvent.class.isAssignableFrom(eventClass)) {
-			return (evt) -> ((PlayerEvent) evt).getPlayer();
+			return (evt) -> ((PlayerEvent) evt).getPlayer().getUniqueId();
 		}
 		if (eventClass == ReceivedPacketEvent.class) {
-			return (evt) -> ((ReceivedPacketEvent) evt).getNessPlayer().getPlayer();
+			return (evt) -> ((ReceivedPacketEvent) evt).getNessPlayer().getUUID();
 		}
 		/*
 		 * Bukkit does not implement some sort of "IPlayerEvent" on events with getPlayer
@@ -71,11 +80,13 @@ class ListeningCheckFactory<E extends Event, C extends AbstractCheck<E>> extends
 		try {
 			Method getPlayerMethod = eventClass.getMethod("getPlayer");
 			return (evt) -> {
+				Player player;
 				try {
-					return (Player) getPlayerMethod.invoke(evt);
+					player = (Player) getPlayerMethod.invoke(evt);
 				} catch (IllegalAccessException | InvocationTargetException ex) {
 					throw new UncheckedReflectiveOperationException(ex);
 				}
+				return player.getUniqueId();
 			};
 		} catch (NoSuchMethodException ignored) {}
 		return null;
