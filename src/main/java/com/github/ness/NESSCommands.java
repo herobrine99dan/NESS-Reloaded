de8d1a7d76400cf87fbfdd063b3f639fc0ee1e96
A248 <anandebeh@gmail.com>
2020-09-23 15:16:44 -0400

    Cleanup NESSCommands and migrate it to new config

--- src/main/java/com/github/ness/NESSCommands.java
+++ src/main/java/com/github/ness/NESSCommands.java
@@ -1,24 +1,27 @@
 package com.github.ness;
 
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.github.ness.gui.ViolationGUI;
-import lombok.AllArgsConstructor;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-@AllArgsConstructor
-public class NESSCommands implements CommandExecutor {
+class NESSCommands implements CommandExecutor {
 
 	private static final Logger logger = NessLogger.getLogger(NESSCommands.class);
+	
 	private final NESSAnticheat ness;
+	
+	NESSCommands(NESSAnticheat ness) {
+		this.ness = ness;
+	}
 
 	private String getPermission(String arg) {
 		if (arg == null) {
@@ -29,8 +32,8 @@ public class NESSCommands implements CommandExecutor {
 			return "ness.command.reload";
 		case "report":
 			return "ness.command.report";
-		case "vl":
-			return "ness.command.vl";
+		case "violations":
+			return "ness.command.violations";
 		case "clear":
 			return "ness.command.clear";
 		case "debug":
@@ -39,117 +42,108 @@ public class NESSCommands implements CommandExecutor {
 			return "ness.command.gui";
 		case "debugvelocity":
 			return "ness.command.debugvelocity";
-		case "mouserecord":
-			return "ness.command.mouserecord";
 		default:
 			return "ness.command.version";
 		}
 	}
-
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		String firstArg = (args.length >= 1) ? args[0] : null;
-		if (sender.hasPermission(getPermission(firstArg))) {
-			if (firstArg == null) {
-				usage(sender);
-			} else {
-				switch (args[0].toLowerCase()) {
-				case "reload":
-					ness.getConfigManager().reload().thenCompose((ignore) -> ness.getCheckManager().reload())
-							.whenComplete((ignore, ex) -> {
-						if (ex != null) {
-							logger.log(Level.WARNING, "Error while reloading", ex);
-							sendMessage(sender, "&cError reloading NESS, check your server console for details.");
-							return;
-						}
-						sendMessage(sender, "&aReloaded NESS!");
-					});
-					break;
-				case "violations":
-					showViolations(sender, (args.length >= 2) ? Bukkit.getPlayer(args[1]) : null);
-					break;
-				case "clear":
-					Player target = (args.length >= 2) ? Bukkit.getPlayer(args[1]) : null;
-					if (target == null) {
-						sendUnknownTarget(sender);
-						break;
-					}
-					NessPlayer nessPlayer = ness.getCheckManager().getExistingPlayer(target);
-					if (nessPlayer == null) {
-						sendUnknownTarget(sender);
-						break;
-					}
-					nessPlayer.checkViolationCounts.clear();
-					sendMessage(sender, "&7Cleared violations for &e%TARGET%".replace("%TARGET%", target.getName()));
-					break;
-				case "version":
-					sendMessage(sender, "&7NESS Version: " + ness.getDescription().getVersion());
-					break;
-				case "gui":
-					if (sender instanceof Player) {
-						new ViolationGUI(sender).createGUI();
-					}
-					break;
-				case "debug":
-					if (sender instanceof Player) {
-						NessPlayer np = ness.getCheckManager().getExistingPlayer((Player) sender);
-						if (np == null) {
-							// This shouldn't happen, but just in case
-							sendMessage(sender, "Your NESSPlayer isn't loaded");
-							return true;
-						}
-						if (np.isDebugMode()) {
-							this.sendMessage(sender, "&7Debug Mode &cDisabled&7!");
-							np.setDebugMode(false);
-						} else {
-							this.sendMessage(sender, "&7Debug Mode &aEnabled&7!");
-							np.setDebugMode(true);
-						}
-					} else {
-						this.sendMessage(sender, "&7Sorry but to use this command you have to be a Player");
-					}
-					break;
-				case "debugvelocity":
-					if (sender instanceof Player) {
-						Player p = (Player) sender;
-						if (args.length > 1) {
-							p.setVelocity(p.getLocation().getDirection().multiply(Double.valueOf(args[1])));
-							p.sendMessage("Done!");
-						}
-					} else {
-						this.sendMessage(sender, "&7Sorry but to use this command you have to be a Player");
-					}
-					break;
-				case "mouserecord":
-					if (sender instanceof Player) {
-						NessPlayer np = ness.getCheckManager().getExistingPlayer((Player) sender);
-						if (np == null) {
-							// This shouldn't happen, but just in case
-							sendMessage(sender, "Your NESSPlayer isn't loaded");
-							return true;
-						}
-						if (np.isMouseRecord()) {
-							this.sendMessage(sender, "&7You've stopped the Mouse Record Process!");
-							np.mouseRecordValues.clear();
-							np.setMouseRecord(false);
-						} else {
-							this.sendMessage(sender, "&7Recording Mouse Movements");
-							np.setMouseRecord(true);
-						}
-					} else {
-						this.sendMessage(sender, "&7Sorry but to use this command you have to be a Player");
-					}
-					break;
-				default:
-					usage(sender);
-					break;
-				}
-			}
-		} else {
-			sendMessage(sender, ness.getNessConfig().getMessages().getString("commands.no-permission",
-					"&cSorry, you cannot use this."));
-		}
+		onCommand0(sender, args);
 		return true;
+	}
+
+	private void onCommand0(final CommandSender sender, final String[] args) {
+		String firstArg = (args.length >= 1) ? args[0] : null;
+		if (!sender.hasPermission(getPermission(firstArg))) {
+			sendMessage(sender, ness.getMessagesConfig().noPermission());
+			return;
+		}
+		if (firstArg == null) {
+			usage(sender);
+			return;
+		}
+		switch (args[0].toLowerCase()) {
+		case "reload":
+			ness.getConfigManager().reload().thenCompose((ignore) -> ness.getCheckManager().reload())
+					.whenComplete((ignore, ex) -> {
+				if (ex != null) {
+					logger.log(Level.WARNING, "Error while reloading", ex);
+					sendMessage(sender, "&cError reloading NESS, check your server console for details.");
+					return;
+				}
+				sendMessage(sender, "&aReloaded NESS!");
+			});
+			break;
+		case "violations":
+			showViolations(sender, (args.length >= 2) ? Bukkit.getPlayer(args[1]) : null);
+			break;
+		case "clear":
+			clearViolations(sender, (args.length >= 2) ? Bukkit.getPlayer(args[1]) : null);
+			break;
+		case "version":
+			sendMessage(sender, "&7NESS Version: " + ness.getDescription().getVersion());
+			break;
+		case "gui":
+			if (sender instanceof Player) {
+				new ViolationGUI(sender).createGUI();
+			}
+			break;
+		case "debug":
+			if (sender instanceof Player) {
+				NessPlayer np = ness.getCheckManager().getExistingPlayer((Player) sender);
+				if (np == null) {
+					// This shouldn't happen, but just in case
+					sendMessage(sender, "Your NESSPlayer isn't loaded");
+					return;
+				}
+				if (np.isDebugMode()) {
+					this.sendMessage(sender, "&7Debug Mode &cDisabled&7!");
+					np.setDebugMode(false);
+				} else {
+					this.sendMessage(sender, "&7Debug Mode &aEnabled&7!");
+					np.setDebugMode(true);
+				}
+			} else {
+				mustBePlayer(sender);
+			}
+			break;
+		case "debugvelocity":
+			if (sender instanceof Player) {
+				Player p = (Player) sender;
+				if (args.length > 1) {
+					p.setVelocity(p.getLocation().getDirection().multiply(Double.valueOf(args[1])));
+					p.sendMessage("Done!");
+				}
+			} else {
+				mustBePlayer(sender);
+			}
+			break;
+		case "mouserecord":
+			if (sender instanceof Player) {
+				NessPlayer np = ness.getCheckManager().getExistingPlayer((Player) sender);
+				if (np == null) {
+					// This shouldn't happen, but just in case
+					sendMessage(sender, "Your NESSPlayer isn't loaded");
+					return;
+				}
+				if (np.isMouseRecord()) {
+					this.sendMessage(sender, "&7You've stopped the Mouse Record Process!");
+					np.mouseRecordValues.clear();
+					np.setMouseRecord(false);
+				} else {
+					this.sendMessage(sender, "&7Recording Mouse Movements");
+					np.setMouseRecord(true);
+				}
+			} else {
+				mustBePlayer(sender);
+			}
+			break;
+		default:
+			usage(sender);
+			break;
+		}
+		return;
 	}
 
 	private void showViolations(CommandSender sender, Player target) {
@@ -163,8 +157,8 @@ public class NESSCommands implements CommandExecutor {
 			return;
 		}
 		String name = target.getName();
-		String header = ness.getMessagesConfig().commands().showViolationsHeader();
-		String body = ness.getMessagesConfig().commands().showViolationsBody();
+		String header = ness.getMessagesConfig().showViolationsHeader();
+		String body = ness.getMessagesConfig().showViolationsBody();
 
 		Map<String, Integer> violationMap = nessPlayer.checkViolationCounts;
 		sendMessage(sender, header.replace("%TARGET%", name));
@@ -174,9 +168,26 @@ public class NESSCommands implements CommandExecutor {
 		}
 	}
 	
+	private void clearViolations(CommandSender sender, Player target) {
+		if (target == null) {
+			sendUnknownTarget(sender);
+			return;
+		}
+		NessPlayer nessPlayer = ness.getCheckManager().getExistingPlayer(target);
+		if (nessPlayer == null) {
+			sendUnknownTarget(sender);
+			return;
+		}
+		nessPlayer.checkViolationCounts.clear();
+		sendMessage(sender, "&7Cleared violations for &e%TARGET%".replace("%TARGET%", target.getName()));
+	}
+	
 	private void sendUnknownTarget(CommandSender sender) {
-		sendMessage(sender, ness.getNessConfig().getMessages().getString("commands.not-found-player",
-				"&cTarget player not specified, not found, or offline."));
+		sendMessage(sender, ness.getMessagesConfig().notFoundPlayer());
+	}
+	
+	private void mustBePlayer(CommandSender sender) {
+		sendMessage(sender, "&7Sorry but to use this command you have to be a Player");
 	}
 
 	private void usage(CommandSender sender) {
--- src/main/java/com/github/ness/config/NessMessages.java
+++ src/main/java/com/github/ness/config/NessMessages.java
@@ -2,27 +2,23 @@ package com.github.ness.config;
 
 import space.arim.dazzleconf.annote.ConfDefault.DefaultString;
 import space.arim.dazzleconf.annote.ConfKey;
-import space.arim.dazzleconf.annote.SubSection;
 
 public interface NessMessages {
 
-	@SubSection
-	Commands commands();
+	@ConfKey("no-permission")
+	@DefaultString("&cSorry, you cannot use this.")
+	String noPermission();
 	
-	interface Commands {
-		
-		@ConfKey("not-found-player")
-		@DefaultString("&cTarget player not specified, not found, or offline.")
-		String notFoundPlayer();
-		
-		@ConfKey("show-violations.header")
-		@DefaultString("&7Violations for &e%TARGET%")
-		String showViolationsHeader();
-		
-		@ConfKey("show-violations.body")
-		@DefaultString("&7Hack: &e%HACK%&7. Count: %VIOLATIONS%")
-		String showViolationsBody();
-		
-	}
+	@ConfKey("not-found-player")
+	@DefaultString("&cTarget player not specified, not found, or offline.")
+	String notFoundPlayer();
+	
+	@ConfKey("show-violations.header")
+	@DefaultString("&7Violations for &e%TARGET%")
+	String showViolationsHeader();
+	
+	@ConfKey("show-violations.body")
+	@DefaultString("&7Hack: &e%HACK%&7. Count: %VIOLATIONS%")
+	String showViolationsBody();
 	
 }
