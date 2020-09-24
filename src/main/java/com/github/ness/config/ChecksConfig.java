724640dc7ea9bcf751b6b8f1380b849fbeebfb6e
A248 <anandebeh@gmail.com>
2020-09-24 09:51:09 -0400

    Migrate AutoClick to new configuration setup

--- src/main/java/com/github/ness/config/ChecksConfig.java
+++ src/main/java/com/github/ness/config/ChecksConfig.java
@@ -1,5 +1,22 @@
 package com.github.ness.config;
 
+import com.github.ness.check.combat.AutoClick;
+
+import space.arim.dazzleconf.annote.ConfComments;
+import space.arim.dazzleconf.annote.ConfKey;
+
 public interface ChecksConfig {
 
+	@ConfKey("autoclick")
+	@ConfComments({
+		"",
+		"AutoClick",
+		"Caps clicks per second (CPS) at a hard limit, also calculates",
+		"the variance in the user's clicks (constancy) and the variance",
+		"in the variance (constancy super).",
+		"",
+		"Performance impact: Minimal",
+		""})
+	AutoClick.CheckConf autoClick();
+	
 }
