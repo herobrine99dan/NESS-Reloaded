735f4730906350efb091f31610b5086d3b4b247b
A248 <anandebeh@gmail.com>
2020-09-24 19:56:41 -0400

    Finish updating NESSExample

--- src/test/java/com/github/ness/api/example/NESSExample.java
+++ src/test/java/com/github/ness/api/example/NESSExample.java
@@ -1,18 +1,13 @@
 package com.github.ness.api.example;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.ness.NessPlayer;
 import com.github.ness.api.AnticheatPlayer;
 import com.github.ness.api.NESSApi;
 import com.github.ness.api.PlayerFlagEvent;
-import com.github.ness.api.Violation;
-import com.github.ness.api.impl.PlayerPunishEvent;
 
 public class NESSExample extends JavaPlugin implements Listener {
 	
@@ -27,20 +22,25 @@ public class NESSExample extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler
-	public void onMove(PlayerMoveEvent e) {
-		if (e.getTo().distanceSquared(e.getFrom()) > 1) {
-			api.flagHack(new Violation("Speed", "HighDistance"), e.getPlayer());
-		}
-	}
-
-	@EventHandler
 	public void onViolation(PlayerFlagEvent evt) {
-		String cheatName = evt.getCheck().getCheckName();
 
-		evt.setCancelled(true);
+		// NESS has bypass permissions built-in and better integrated
+		// This is just an example
 
 		AnticheatPlayer anticheatPlayer = evt.getPlayer();
-		Player player = anticheatPlayer.getPlayer();
+		String permission = "anticheat.bypass." + evt.getCheck().getCheckName();
+
+		boolean hasBypassPermission;
+		if (evt.isAsynchronous()) {
+			// hasPermission is not necessarily thread safe
+			// this would need to either resynchronise or detect thread-safe permissions plugins
+			hasBypassPermission = false;
+		} else {
+			hasBypassPermission = anticheatPlayer.getPlayer().hasPermission(permission);
+		}
+		if (hasBypassPermission) {
+			evt.setCancelled(true);
+		}
 	}
 
 }
