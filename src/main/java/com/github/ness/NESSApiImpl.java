18e0f7445e5780906d069939861ce5410b10cb02
A248 <anandebeh@gmail.com>
2020-09-25 09:09:20 -0400

    Support deprecated NESSApi#flagHack method

--- src/main/java/com/github/ness/NESSApiImpl.java
+++ src/main/java/com/github/ness/NESSApiImpl.java
@@ -2,6 +2,7 @@ package com.github.ness;
 
 import java.util.Collection;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import com.github.ness.api.AnticheatCheck;
 import com.github.ness.api.AnticheatPlayer;
@@ -13,33 +14,44 @@ import org.bukkit.entity.Player;
 
 final class NESSApiImpl implements NESSApi {
 
-    private final NESSAnticheat ness;
-    
-    NESSApiImpl(NESSAnticheat ness) {
-    	this.ness = ness;
-    }
-    
+	private final NESSAnticheat ness;
+
+	NESSApiImpl(NESSAnticheat ness) {
+		this.ness = ness;
+	}
+
 	@SuppressWarnings("deprecation")
 	@Deprecated
 	@Override
 	public void addViolationAction(com.github.ness.api.ViolationAction action) {
 		addViolationTrigger(ViolationMigratorUtil.triggerForAction(action));
 	}
-	
-    @Override
-    public void addViolationTrigger(ViolationTrigger trigger) {
-        ness.getViolationManager().addTrigger(trigger);
-    }
 
-    @SuppressWarnings("deprecation")
 	@Override
-    public void flagHack(com.github.ness.api.Violation violation, Player player) {
-        NessPlayer nessPlayer = ness.getCheckManager().getExistingPlayer(player);
-        if (nessPlayer == null) {
-        	return;
-        }
-        nessPlayer.setViolation(violation);
-    }
+	public void addViolationTrigger(ViolationTrigger trigger) {
+		ness.getViolationManager().addTrigger(trigger);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void flagHack(com.github.ness.api.Violation violation, Player player) {
+		LoggerHolder.logger.warning(
+				"Caller is extremely discouraged from using the deprecated flagHack(Violation, Player) method");
+		AnticheatPlayer anticheatPlayer = getPlayer(player.getUniqueId());
+		if (anticheatPlayer != null) {
+			for (AnticheatCheck check : getAllChecks()) {
+				if (check.getCheckName().equals(violation.getCheck())) {
+					check.flagHack(anticheatPlayer);
+					return;
+				}
+			}
+
+		}
+	}
+	
+	private static class LoggerHolder {
+		static final Logger logger = NessLogger.getLogger(NESSApiImpl.class);
+	}
 
 	@Override
 	public Collection<? extends AnticheatCheck> getAllChecks() {
