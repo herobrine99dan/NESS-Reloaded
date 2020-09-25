2b8adda39fca9880e58413ec5ea71be8ace44034
A248 <anandebeh@gmail.com>
2020-09-25 16:34:15 -0400

    Improve code formatting Killaura check

--- src/main/java/com/github/ness/check/combat/Killaura.java
+++ src/main/java/com/github/ness/check/combat/Killaura.java
@@ -66,102 +66,97 @@ public class Killaura extends ListeningCheck<EntityDamageByEntityEvent> {
 		Check6(e);
 	}
 
-	public void Check(EntityDamageByEntityEvent e) {
-		if (e.getDamager() instanceof Player) {
-			Player player = (Player) e.getDamager();
-			Entity entity = e.getEntity();
-			NessPlayer np = player();
-			double range = Math.hypot(np.getMovementValues().getTo().getX() - entity.getLocation().getX(),
-					np.getMovementValues().getTo().getZ() - entity.getLocation().getZ());
-			double maxReach = this.maxReach;
-			if (player.getGameMode().equals(GameMode.CREATIVE)) {
-				maxReach = 5.5D;
-			}
-			if (Utility.specificBlockNear(e.getDamager().getLocation(), "water") || MathUtils
-					.yawTo180F(np.getMovementValues().getTo().getYaw() - entity.getLocation().getYaw()) <= 90) {
-				maxReach += 0.4D;
-			}
-			maxReach += (Utility.getPing(player) / 100) / 15;
-			if ((range > maxReach && range < 6.5D)
-					|| Utility.getDistance3D(player.getLocation(), entity.getLocation()) > 5) {
-				this.punish(e, player, "Reach: " + range);
-			}
+	public void Check(EntityDamageByEntityEvent eventt) {
+		Player player = (Player) eventt.getDamager();
+		Entity entity = eventt.getEntity();
+		NessPlayer np = player();
+		double range = Math.hypot(np.getMovementValues().getTo().getX() - entity.getLocation().getX(),
+				np.getMovementValues().getTo().getZ() - entity.getLocation().getZ());
+		double maxReach = this.maxReach;
+		if (player.getGameMode().equals(GameMode.CREATIVE)) {
+			maxReach = 5.5D;
+		}
+		if (Utility.specificBlockNear(eventt.getDamager().getLocation(), "water") || MathUtils
+				.yawTo180F(np.getMovementValues().getTo().getYaw() - entity.getLocation().getYaw()) <= 90) {
+			maxReach += 0.4D;
+		}
+		maxReach += (Utility.getPing(player) / 100) / 15;
+		if ((range > maxReach && range < 6.5D)
+				|| Utility.getDistance3D(player.getLocation(), entity.getLocation()) > 5) {
+			punish(eventt, "Reach: " + range);
 		}
 	}
 
-	public void Check1(EntityDamageByEntityEvent e) {
-		Player p = (Player) e.getDamager();
-		final Location loc = p.getLocation();
+	public void Check1(EntityDamageByEntityEvent event) {
+		Player player = (Player) event.getDamager();
+		final Location loc = player.getLocation();
 
 		runTaskLater(() -> {
-			Location loc1 = p.getLocation();
+			Location loc1 = player.getLocation();
 			float grade = loc.getYaw() - loc1.getYaw();
 			if (Math.round(grade) > maxYaw) {
-				punish(e, p, "HighYaw " + grade);
+				punish(event, "HighYaw " + grade);
 			}
 		}, durationOfTicks(2));
 	}
 
 	public void Check2(EntityDamageByEntityEvent event) {
-		if (event.getDamager() instanceof Player) {
-			Player player = (Player) event.getDamager();
-			if (player.getLocation().getPitch() == Math.round(player.getLocation().getPitch())) {
-				punish(event, player, "PerfectAngle");
-			} else if (player.getLocation().getYaw() == Math.round(player.getLocation().getYaw())) {
-				punish(event, player, "PerfectAngle1");
-			} else if (player.isDead()) {
-				punish(event, player, "Impossible");
-			}
+		Player player = (Player) event.getDamager();
+		if (player.getLocation().getPitch() == Math.round(player.getLocation().getPitch())) {
+			punish(event, "PerfectAngle");
+		} else if (player.getLocation().getYaw() == Math.round(player.getLocation().getYaw())) {
+			punish(event, "PerfectAngle1");
+		} else if (player.isDead()) {
+			punish(event, "Impossible");
 		}
 	}
 
-	public void Check3(EntityDamageByEntityEvent e) {
-		if (e.getDamager() instanceof Player) {
-			Player p = (Player) e.getDamager();
-			if (!p.hasLineOfSight(e.getEntity())) {
-				Block b = p.getTargetBlock(null, 5);
-				if (b.getType().isSolid() && b.getType().isOccluding()) {
-					punish(e, p, "WallHit");
-				}
-			}
+	public void Check3(EntityDamageByEntityEvent event) {
+		Player player = (Player) event.getDamager();
+		if (player.hasLineOfSight(event.getEntity())) {
+			return;
+		}
+		Block b = player.getTargetBlock(null, 5);
+		if (b.getType().isSolid() && b.getType().isOccluding()) {
+			punish(event, "WallHit");
 		}
 	}
 
-	public void Check4(EntityDamageByEntityEvent e) {
-		if (e.getDamager() instanceof Player && e.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
-			if (e.getEntity().getEntityId() == e.getDamager().getEntityId()) {
-				punish(e, (Player) e.getDamager(), "SelfHit");
-			}
+	public void Check4(EntityDamageByEntityEvent event) {
+		if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK
+				&& event.getEntity().getEntityId() == event.getDamager().getEntityId()) {
+			punish(event, "SelfHit");
 		}
 	}
 
-	public void Check5(EntityDamageByEntityEvent e) {
-		if (e.getDamager() instanceof Player) {
-			if (e.getEntity() instanceof LivingEntity && Bukkit.getVersion().contains("1.8")) {
-				NessPlayer nessPlayer = player();
-				nessPlayer.attackedEntities.add(e.getEntity().getEntityId());
-				if (nessPlayer.attackedEntities.size() > 2) {
-					punish(e, (Player) e.getDamager(), "MultiAura Entities: " + nessPlayer.attackedEntities.size());
-				}
-			}
+	public void Check5(EntityDamageByEntityEvent eventt) {
+		if (!(eventt.getEntity() instanceof LivingEntity)) {
+			return;
+		}
+		if (!Bukkit.getVersion().contains("1.8")) {
+			return;
+		}
+		NessPlayer nessPlayer = player();
+		nessPlayer.attackedEntities.add(eventt.getEntity().getEntityId());
+		if (nessPlayer.attackedEntities.size() > 2) {
+			punish(eventt, "MultiAura Entities: " + nessPlayer.attackedEntities.size());
 		}
 	}
 
-	public void Check6(EntityDamageByEntityEvent e) {
-		if (e.getDamager() instanceof Player) {
-			if (e.getEntity() instanceof LivingEntity) {
-				NessPlayer nessPlayer = player();
-				double angle = Utility.getAngle((Player) e.getDamager(), e.getEntity().getLocation(),
-						nessPlayer.getMovementValues().getTo().getDirectionVector());
-				if (angle < -0.4) {
-					punish(e, (Player) e.getDamager(), "HitBox");
-				}
-			}
+	public void Check6(EntityDamageByEntityEvent event) {
+		if (!(event.getEntity() instanceof LivingEntity)) {
+			return;
+		}
+		NessPlayer nessPlayer = player();
+		double angle = Utility.getAngle((Player) event.getDamager(), event.getEntity().getLocation(),
+				nessPlayer.getMovementValues().getTo().getDirectionVector());
+		if (angle < -0.4) {
+			punish(event, "HitBox");
 		}
 	}
 
-	private void punish(EntityDamageByEntityEvent evt, Player p, String module) {
-		flagEvent(evt, module);
+	private void punish(EntityDamageByEntityEvent event, String module) {
+		flagEvent(event, module);
 	}
 
 }
