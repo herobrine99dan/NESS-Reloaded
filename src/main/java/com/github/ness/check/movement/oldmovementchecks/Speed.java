9b9966e97de37bde7585edb9024064ac7e8fadf4
herobrine99dan <radiojukebox05@gmail.com>
2020-12-22 16:05:49 +0100

    Fixing Speed False Flag

--- src/main/java/com/github/ness/check/movement/oldmovementchecks/Speed.java
+++ src/main/java/com/github/ness/check/movement/oldmovementchecks/Speed.java
@@ -17,73 +17,76 @@ import com.github.ness.utility.Utility;
 
 public class Speed extends ListeningCheck<PlayerMoveEvent> {
 
-	public static final ListeningCheckInfo<PlayerMoveEvent> checkInfo = CheckInfos.forEvent(PlayerMoveEvent.class);
+    public static final ListeningCheckInfo<PlayerMoveEvent> checkInfo = CheckInfos.forEvent(PlayerMoveEvent.class);
 
-	public Speed(ListeningCheckFactory<?, PlayerMoveEvent> factory, NessPlayer player) {
-		super(factory, player);
-	}
+    public Speed(ListeningCheckFactory<?, PlayerMoveEvent> factory, NessPlayer player) {
+        super(factory, player);
+    }
 
-	@Override
-	protected void checkEvent(PlayerMoveEvent e) {
-		Location to = e.getTo();
-		Location from = e.getFrom();
-		Player player = e.getPlayer();
-		NessPlayer nessPlayer = this.player();
-		MovementValues movementValues = this.player().getMovementValues();
-		final double dist = from.distance(to);
-		double hozDist = dist - (to.getY() - from.getY());
-		if (to.getY() < from.getY())
-			hozDist = dist - (from.getY() - to.getY());
-		double maxSpd = player.getWalkSpeed() * 2.15; // 0.43
-		if (player().milliSecondTimeDifference(PlayerAction.VELOCITY) < 1800) {
-			hozDist -= Math.abs(player().getLastVelocity().getX());
-			hozDist -= Math.abs(player().getLastVelocity().getZ());
-		}
-		if (player.hasPotionEffect(PotionEffectType.SPEED)) {
-			final int level = Utility.getPotionEffectLevel(player, PotionEffectType.SPEED);
-			hozDist -= (float) (hozDist / 100.0D * level * 20.0D);
-		}
-		Material mat = null;
-		for (int x = -1; x < 1; x++) {
-			for (int z = -1; z < 1; z++) {
-				mat = from.getWorld()
-						.getBlockAt(from.getBlockX() + x, player.getEyeLocation().getBlockY() + 1, from.getBlockZ() + z)
-						.getType();
-				if (mat.isSolid()) {
-					maxSpd = player.getWalkSpeed() * 2.535; // 0.507
-					break;
-				}
-			}
-		}
-		if (movementValues.isAroundSlabs() || movementValues.isAroundStairs()) {
-			maxSpd += player.getWalkSpeed() * 0.25;
-		}
-		if (movementValues.isAroundIce() || nessPlayer.getTimeSinceLastWasOnIce() < 1000) {
-			maxSpd += player.getWalkSpeed();
-		}
-		if (movementValues.hasBlockNearHead()) {
-			maxSpd += player.getWalkSpeed() * 0.45;
-		}
-		if (player.isInsideVehicle() && player.getVehicle().getType().name().contains("BOAT"))
-			maxSpd = 2.787;
-		if (hozDist > maxSpd && !player.isFlying() && !player.getAllowFlight()
-				&& nessPlayer.milliSecondTimeDifference(PlayerAction.DAMAGE) >= 2000 && !nessPlayer.isTeleported()) {
-			if (nessPlayer.getMovementValues().isGroundAround()) {
-				if (nessPlayer.getTimeSinceLastWasOnIce() >= 1000) {
-					if (!player.isInsideVehicle()
-							|| (player.isInsideVehicle() && !player.getVehicle().getType().name().contains("HORSE"))) {
-						Material small = player.getWorld().getBlockAt(player.getLocation().subtract(0, .1, 0))
-								.getType();
-						if (!player.getWorld().getBlockAt(from).getType().isSolid()
-								&& !player.getWorld().getBlockAt(to).getType().isSolid()
-								&& nessPlayer.milliSecondTimeDifference(PlayerAction.BLOCKPLACED) > 1000) {
-							if (!small.name().contains("TRAPDOOR")) {
-								this.flagEvent(e, maxSpd + " Dist: " + hozDist);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    @Override
+    protected void checkEvent(PlayerMoveEvent e) {
+        Location to = e.getTo();
+        Location from = e.getFrom();
+        Player player = e.getPlayer();
+        NessPlayer nessPlayer = this.player();
+        MovementValues movementValues = this.player().getMovementValues();
+        final double dist = from.distance(to);
+        double hozDist = dist - (to.getY() - from.getY());
+        if (to.getY() < from.getY())
+            hozDist = dist - (from.getY() - to.getY());
+        double maxSpd = player.getWalkSpeed() * 2.15; // 0.43
+        final double velocity = Math.hypot(player().getLastVelocity().getX(), player().getLastVelocity().getZ());
+        if (player().milliSecondTimeDifference(PlayerAction.VELOCITY) < 2000) {
+            hozDist -= velocity;
+        }
+        if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+            final int level = Utility.getPotionEffectLevel(player, PotionEffectType.SPEED);
+            hozDist -= (float) (hozDist / 100.0D * level * 20.0D);
+        }
+        Material mat = null;
+        for (int x = -1; x < 1; x++) {
+            for (int z = -1; z < 1; z++) {
+                mat = from.getWorld()
+                        .getBlockAt(from.getBlockX() + x, player.getEyeLocation().getBlockY() + 1, from.getBlockZ() + z)
+                        .getType();
+                if (mat.isSolid()) {
+                    maxSpd = player.getWalkSpeed() * 2.535; // 0.507
+                    break;
+                }
+            }
+        }
+        if (movementValues.isAroundSlabs() || movementValues.isAroundStairs()) {
+            maxSpd += player.getWalkSpeed() * 0.25;
+        }
+        if (movementValues.isAroundIce() || nessPlayer.getTimeSinceLastWasOnIce() < 1000) {
+            maxSpd += player.getWalkSpeed();
+        }
+        if (movementValues.hasBlockNearHead()) {
+            maxSpd += player.getWalkSpeed() * 0.45;
+        }
+        if (movementValues.isAroundSlime()) {
+            maxSpd += velocity;
+        }
+        if (player.isInsideVehicle() && player.getVehicle().getType().name().contains("BOAT"))
+            maxSpd = 2.787;
+        if (hozDist > maxSpd && !player.isFlying() && !player.getAllowFlight()
+                && nessPlayer.milliSecondTimeDifference(PlayerAction.DAMAGE) >= 2000 && !nessPlayer.isTeleported()) {
+            if (nessPlayer.getMovementValues().isGroundAround()) {
+                if (nessPlayer.getTimeSinceLastWasOnIce() >= 1000) {
+                    if (!player.isInsideVehicle()
+                            || (player.isInsideVehicle() && !player.getVehicle().getType().name().contains("HORSE"))) {
+                        Material small = player.getWorld().getBlockAt(player.getLocation().subtract(0, .1, 0))
+                                .getType();
+                        if (!player.getWorld().getBlockAt(from).getType().isSolid()
+                                && !player.getWorld().getBlockAt(to).getType().isSolid()
+                                && nessPlayer.milliSecondTimeDifference(PlayerAction.BLOCKPLACED) > 1000) {
+                            if (!small.name().contains("TRAPDOOR")) {
+                                this.flagEvent(e, maxSpd + " Dist: " + hozDist);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/main/java/com/github/ness/utility/raytracer/RayCaster.java
+++ src/main/java/com/github/ness/utility/raytracer/RayCaster.java
@@ -1,38 +1,51 @@
 package com.github.ness.utility.raytracer;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import com.github.ness.utility.Utility;
 
 public class RayCaster {
 
-	Location origin;
-	double maxDistance;
-	Player player;
+    private Location origin;
+    private double maxDistance;
+    private Player player;
+    private Block blockFounded;
+    private Entity entityFounded;
 
-	public RayCaster(Player player, Location origin, double maxDistance) {
-		this.origin = origin;
-		this.player = player;
-		this.maxDistance = maxDistance;
-	}
+    public RayCaster(Player player, Location origin, double maxDistance) {
+        this.origin = origin;
+        this.player = player;
+        this.maxDistance = maxDistance;
+    }
 
-	public void compute(RayCaster.RaycastType type) {
-		if (type == RayCaster.RaycastType.ENTITY) {
-			for (Entity e : this.origin.getWorld().getNearbyEntities(origin, maxDistance, maxDistance, maxDistance)) {
-				if (e instanceof LivingEntity) {
-					Utility.getAngle(player, e.getLocation(), origin.getDirection());
-				}
-			}
-		} else if (type == RayCaster.RaycastType.BLOCK) {
-			
-		}
-	}
+    public void compute(RayCaster.RaycastType type) {
+        if (type == RayCaster.RaycastType.ENTITY) {
+            for (Entity e : this.origin.getWorld().getNearbyEntities(origin, maxDistance, maxDistance, maxDistance)) {
+                if (e instanceof LivingEntity) {
+                    if(player.hasLineOfSight(e)) {
+                        entityFounded = e;
+                    }
+                }
+            }
+        } else if (type == RayCaster.RaycastType.BLOCK) {
+            Location loc = player.getEyeLocation();
+            Vector v = loc.getDirection().normalize();
+            for (double i = 1; i <= maxDistance; i += 0.1) {
+                loc.add(v);
+                if (loc.getBlock().getType().isOccluding()) {
+                    blockFounded = loc.getBlock();
+                }
+            }
+        }
+    }
 
-	public static enum RaycastType {
-		ENTITY, BLOCK;
-	}
+    public static enum RaycastType {
+        ENTITY, BLOCK;
+    }
 
 }
