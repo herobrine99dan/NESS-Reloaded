126447beb4fe4952d8bd398ba278c1b12154dfa5
A248 <anandebeh@gmail.com>
2020-12-24 10:52:49 -0500

    Use MethodHandle to instantiate Checks

--- src/main/java/com/github/ness/check/CheckInstantiators.java
+++ src/main/java/com/github/ness/check/CheckInstantiators.java
@@ -1,21 +1,31 @@
 package com.github.ness.check;
 
-import com.github.ness.utility.UncheckedReflectiveOperationException;
+import com.github.ness.reflect.ReflectionException;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 final class CheckInstantiators {
 
 	private CheckInstantiators() {}
 
 	static <C extends Check> CheckInstantiator<C> fromConstructor(Constructor<C> constructor) {
+		MethodHandle methodHandle;
+		try {
+			methodHandle = MethodHandles.lookup().unreflectConstructor(constructor);
+		} catch (IllegalAccessException ex) {
+			throw new ReflectionException(ex);
+		}
 		return (factory, nessPlayer) -> {
 			try {
-				return constructor.newInstance(factory, nessPlayer);
-			} catch (InstantiationException | IllegalAccessException | InvocationTargetException ex) {
-				throw new UncheckedReflectiveOperationException(
-						"Unable to instantiate check " + constructor.getDeclaringClass().getName(), ex);
+				@SuppressWarnings("unchecked")
+				C check = (C) methodHandle.invoke(factory, nessPlayer);
+				return check;
+			} catch (RuntimeException | Error ex) {
+				throw ex;
+			} catch (Throwable ex) {
+				throw new ReflectionException("Unable to instantiate check", ex);
 			}
 		};
 	}
