9aec2385185a960254becbb8beb08ff46cfbe2cf
A248 <anandebeh@gmail.com>
2021-01-09 16:40:11 -0500

    Explain why not to rely on formal return type

--- src/main/java/com/github/ness/packets/NetworkReflectionCreation.java
+++ src/main/java/com/github/ness/packets/NetworkReflectionCreation.java
@@ -20,9 +20,17 @@ public class NetworkReflectionCreation {
 
 	public NetworkReflection create() {
 		Class<?> craftPlayerClass = classLocator.getObcClass("entity.CraftPlayer");
+		/*
+		 * Note: getHandleMethod's formal return type may be either EntityHuman or EntityPlayer.
+		 * Class.getDeclaredMethods has a bug where it includes shadowed methods. As a result, the
+		 * reflection API sometimes returns the shadowed method as a MethodInvoker. The shadowed
+		 * 'getHandle' returns EntityHuman whereas the normal 'getHandle' returns EntityPlayer.
+		 * See https://bugs.openjdk.java.net/browse/JDK-8144122
+		 */
 		MethodHandle getHandleMethod = reflection
 				.getMethod(craftPlayerClass, MemberDescriptions.forMethod("getHandle")).unreflect();
 
+		// Use entityPlayerClass rather than getHandleMethod.type().returnType()
 		Class<?> entityPlayerClass = classLocator.getNmsClass("EntityPlayer");
 		MethodHandle playerConnectionField = reflection
 				.getField(entityPlayerClass, MemberDescriptions.forField("playerConnection"))
