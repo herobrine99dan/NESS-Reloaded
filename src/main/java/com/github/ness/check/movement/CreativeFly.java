d9424f2f936697892ef725896c1ad98aa9144d6e
herobrine99dan <radiojukebox05@gmail.com>
2021-03-13 09:11:03 +0100

    Implementing CreativeFly Check

---
+++ src/main/java/com/github/ness/check/movement/CreativeFly.java
@@ -0,0 +1,47 @@
+package com.github.ness.check.movement;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import com.github.ness.NessPlayer;
+import com.github.ness.check.CheckInfos;
+import com.github.ness.check.ListeningCheck;
+import com.github.ness.check.ListeningCheckFactory;
+import com.github.ness.check.ListeningCheckInfo;
+import com.github.ness.data.MovementValues;
+
+public class CreativeFly extends ListeningCheck<PlayerMoveEvent> {
+
+	public static final ListeningCheckInfo<PlayerMoveEvent> checkInfo = CheckInfos.forEvent(PlayerMoveEvent.class);
+
+	public CreativeFly(ListeningCheckFactory<?, PlayerMoveEvent> factory, NessPlayer player) {
+		super(factory, player);
+	}
+
+	private double lastMotionXZ, lastMotionY;
+
+	@Override
+	protected void checkEvent(PlayerMoveEvent event) {
+		MovementValues values = this.player().getMovementValues();
+		double xzDiff = values.getXZDiff();
+		double yDiff = values.getyDiff();
+		Player player = event.getPlayer();
+		if (player.isFlying()) {
+			double predictedXZ = lastMotionXZ * 0.91f;
+			double predictedY = lastMotionY * 0.6;
+			double resultXZ = Math.abs(xzDiff - predictedXZ);
+			double resultY = Math.abs(yDiff - predictedY);
+			double maxXZ = player.getFlySpeed(); //(player.getFlySpeed() * 0.1) / 0.1 that become player.getFlySpeed()
+			double maxY = (player.getFlySpeed() * 0.38) / 0.1;
+			this.player().sendDevMessage("maxXZ: " + (float) maxXZ + " maxY: " + (float) maxY + " flySpeed:" + (float) player.getFlySpeed());
+			if (resultXZ > maxXZ) {
+				this.flagEvent(event, "resultXZ: " + (float) resultXZ);
+			}
+			if(resultY > maxY && yDiff > 0) {
+				this.flagEvent(event, "resultY: " + (float) resultY);
+			}
+		}
+		this.lastMotionXZ = xzDiff;
+	}
+
+}
