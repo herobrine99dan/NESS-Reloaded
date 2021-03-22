773fb1a036d85945f449ab863f010abf7d1310b8
herobrine99dan <radiojukebox05@gmail.com>
2021-03-22 16:13:01 +0100

    Splitting VerticalFly check

--- src/main/java/com/github/ness/check/movement/oldmovementchecks/VerticalFly1.java
+++ src/main/java/com/github/ness/check/movement/oldmovementchecks/VerticalFly1.java
@@ -10,14 +10,13 @@ import com.github.ness.check.ListeningCheck;
 import com.github.ness.check.ListeningCheckFactory;
 import com.github.ness.check.ListeningCheckInfo;
 import com.github.ness.data.MovementValues;
-import com.github.ness.data.PlayerAction;
 import com.github.ness.utility.Utility;
 
-public class VerticalFly extends ListeningCheck<PlayerMoveEvent> {
+public class VerticalFly1 extends ListeningCheck<PlayerMoveEvent> {
 
 	public static final ListeningCheckInfo<PlayerMoveEvent> checkInfo = CheckInfos.forEvent(PlayerMoveEvent.class);
 
-	public VerticalFly(ListeningCheckFactory<?, PlayerMoveEvent> factory, NessPlayer player) {
+	public VerticalFly1(ListeningCheckFactory<?, PlayerMoveEvent> factory, NessPlayer player) {
 		super(factory, player);
 	}
 
@@ -37,11 +36,13 @@ public class VerticalFly extends ListeningCheck<PlayerMoveEvent> {
 			}
 		}
 		if (values.getTo().getY() > values.getFrom().getY() && !values.isGroundAround()) {
-			if (values.getXZDiff() == 0.0D && !player.hasPotionEffect(PotionEffectType.JUMP) && values.isAroundSlime()
-					&& !values.isAroundCactus()
-					&& this.player().milliSecondTimeDifference(PlayerAction.ATTACK) >= 500.0D) {
-				this.flagEvent(e);
-				return;
+			if (player.getLocation().getY() % 0.5D != 0.0D && !values.isOnGroundCollider()
+					&& !values.isAroundNonOccludingBlocks() && !values.isAroundLiquids() && !values.isAroundLadders()
+					&& !values.isAroundKelp() && !player.hasPotionEffect(PotionEffectType.JUMP)
+					&& !values.isAroundSlime()) {
+				if (++buffer > 2) {
+					this.flagEvent(e);
+				}
 			}
 		} else if (buffer > 0) {
 			buffer -= 0.25;
