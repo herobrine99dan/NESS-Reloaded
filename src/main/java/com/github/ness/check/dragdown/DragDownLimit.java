f0f833d3a59849136ba572044538f30a4c2a3363
MatuloM <71179721+MatuloM@users.noreply.github.com>
2021-03-22 09:50:25 +0100

    Fixing drag down limit
    Now you won't stuck mid air while flagging.

--- src/main/java/com/github/ness/check/dragdown/DragDownLimit.java
+++ src/main/java/com/github/ness/check/dragdown/DragDownLimit.java
@@ -10,6 +10,25 @@ public class DragDownLimit implements SetBack {
 
 	@Override
 	public boolean doSetBack(NessPlayer nessPlayer, Cancellable e) {
+		// Maybe some issues on fences and walls
+		final Location block = nessPlayer.getBukkitPlayer().getLocation().clone();
+		for (int i = 1; i < 10; i++) {
+			block.add(0,-1,0);
+			if (block.getBlock().getType().isSolid()) {
+				block.add(0,0.51,0);
+				if (block.getBlock().getType().isSolid()) {
+					block.add(0,0.5,0);
+				}
+				break;
+			}
+		}
+		if(block.getBlock().getType().isSolid()) {
+			nessPlayer.completeDragDown();
+			return false;
+		}
+		nessPlayer.getBukkitPlayer().teleport(block,TeleportCause.PLUGIN);
+		return true;
+		/*   old drag down limit
 		final Location block = nessPlayer.getBukkitPlayer().getLocation().clone().add(0, -0.5, 0);
 		for (int i = 0; i < 10; i++) {
 			if (block.getBlock().getType().isSolid()) {
@@ -23,7 +42,7 @@ public class DragDownLimit implements SetBack {
 			return false;
 		}
 		nessPlayer.getBukkitPlayer().teleport(block,TeleportCause.PLUGIN);
-		return true;
+		return true;*/
 	}
 
 }
